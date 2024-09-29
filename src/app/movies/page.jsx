319b4de9e632d6bd402b1ec17e1b42202f8eba3b
Andrey <devandrey98@gmail.com>
2024-09-28 22:44:12 -0600

    added media pages

---
+++ src/app/movies/page.jsx
@@ -0,0 +1,10 @@
+import Hero from '@/components/Hero';
+import Slider from '@/components/Slider';
+export default function Movies(){
+    return (
+      <>
+        <Hero />
+        <Slider />
+      </>
+    );
+}
---
+++ src/app/tvshows/page.jsx
@@ -0,0 +1,10 @@
+import Hero from "@/components/Hero";
+import Slider from "@/components/Slider";
+export default function Tvshows() {
+  return (
+    <>
+      <Hero />
+      <Slider />
+    </>
+  );
+}
