03bb6a76d015344410fcd2c38ad1ce18531f9bac
Andrey <devandrey98@gmail.com>
2024-10-25 11:48:02 -0600

    added redirection on logout

--- src/components/UserActions.jsx
+++ src/components/UserActions.jsx
@@ -4,11 +4,14 @@ import Link from "next/link";
 import { Context } from "@/context/Context";
 import AuthForm from "@/components/AuthForm";
 import { auth } from "../firebase/firebase.config";
+import { useRouter } from "next/navigation";
 
 export default function UserActions() {
   const { setAuthModalActive, userLogged, setUserLogged, setFirebaseActiveUser } = useContext(Context);
   const [userData, setUserData] = useState({ username: "", email: "", password: "" });
   const [errorMessage, setErrorMessage] = useState({ active: false, text: "" });
+
+  const router = useRouter();
 
   const handleLogout = async () => {
     auth.signOut().then(() => {
@@ -17,22 +20,23 @@ export default function UserActions() {
       setFirebaseActiveUser({ email: null, uid: null });
       setErrorMessage({ active: false, text: "" });
       setAuthModalActive(false);
+      router.push("/movies");
     });
   };
   return userLogged ? (
-    <>
+    <div className="flex flex-col items-start gap-2">
       <Link
-        href={"/profile"}
+        href={"/lists"}
         onClick={() => {
           setAuthModalActive(false);
         }}
       >
-        <i className="bi bi-person"></i> Profile{" "}
+        <i className="bi bi-person"></i> Lists{" "}
       </Link>
       <Link href={""} onClick={handleLogout}>
         <i className="bi bi-box-arrow-right"></i> Log out
       </Link>
-    </>
+    </div>
   ) : (
     <AuthForm />
   );
