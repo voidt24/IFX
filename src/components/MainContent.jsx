fdc94c08922668ec97c451f6f445af04b4e3b945
Andrey <devandrey98@gmail.com>
2024-10-26 11:35:48 -0600

    renamed variables, added comments and loaders

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -4,18 +4,43 @@ import Hero from "./Hero";
 import Slider from "./Slider";
 import SliderCard from "./SliderCard";
 import { Context } from "../context/Context";
+import { CircularProgress } from "@mui/material";
 
 export default function MainContent() {
-  const { apiData, currentMediaType } = useContext(Context);
-  const [results, setResults] = useState([]);
+  const { apiData, setCurrentId, initialDataError, initialDataIsLoading, currentMediaType } = useContext(Context);
+  const [sliderData, setSliderData] = useState([]);
+
+  useEffect(() => {
+    setCurrentId(undefined);
+  }, []);
 
   useEffect(() => {
     if (apiData && apiData.length > 0) {
       const [trendingResults, popularResults] = apiData[0];
 
-      setResults(currentMediaType == "movies" ? popularResults : trendingResults.slice(5, 20));
+      setSliderData(currentMediaType == "movies" ? popularResults : trendingResults.slice(5, 20)); // check fetchData.js (helper) for context on why we save diff. results based on currentMediaType
+
     }
   }, [apiData, currentMediaType]);
+
+  if (initialDataIsLoading) {
+    return (
+      <span className="flex items-center justify-center h-screen">
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <CircularProgress color="inherit" size={100} />
+        </div>
+      </span>
+    );
+  }
+  if (initialDataError) {
+    return (
+      <div className="error not-found">
+        <h1>ERROR</h1>
+        <p>Please try again</p>
+      </div>
+    );
+  }
+
   return (
     <>
       <Hero />
@@ -28,8 +53,8 @@ export default function MainContent() {
         controls
         expectingCards
       >
-        {results.map((result) => {
-          return <SliderCard result={result} key={result.id} />;
+        {sliderData.map((sliderData) => {
+          return <SliderCard result={sliderData} key={sliderData.id} />;
         })}
       </Slider>
     </>
