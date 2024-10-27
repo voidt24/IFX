486a95ad6c42e46c42b52f34147457fa01bbd478
Andrey <devandrey98@gmail.com>
2024-10-27 12:54:55 -0600

    added cache function to get and save data

---
+++ src/helpers/cache/cache.js
@@ -0,0 +1,39 @@
+import { CACHENAME } from "../api.config";
+
+export const getFromCache = async (url, getFromApi) => {
+  try {
+    const response = await caches.match(url);
+    const expirationResponse = await caches.match(`${url}-expiration`);
+
+    if (response) {
+      const expirationDate = await expirationResponse.json();
+      if (Date.now() > expirationDate.validTime) {
+        const cache = await caches.open(CACHENAME);
+        await cache.delete(url);
+        await cache.delete(`${url}-expiration`);
+        return getFromApi();
+      }
+
+      const json = await response.json();
+      return json;
+    } else {
+      throw new Error();
+    }
+  } catch (e) {
+    throw e;
+  }
+};
+
+export const saveToCache = async (json, url, validTime) => {
+  try {
+    const responseClone = new Response(JSON.stringify(json), {
+      headers: { "Content-Type": "application/json" },
+    });
+
+    const cache = await caches.open(CACHENAME);
+    await cache.put(url, responseClone);
+    await cache.put(`${url}-expiration`, new Response(JSON.stringify({ headers: { "Content-Type": "application/json" }, validTime })));
+  } catch (e) {
+    //no actions on this exception
+  }
+};
