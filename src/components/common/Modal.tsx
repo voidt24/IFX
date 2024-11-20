8021303a29bf4cfab64f74d7a9a9cd0cddedfb1c
Andrey <devandrey98@gmail.com>
2024-11-20 01:24:37 -0600

    updates style

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -11,13 +11,13 @@ const Modal = ({ modalActive, setModalActive, children }: ModalProps) => {
     <>
       <div className={`flex fixed z-[99999] h-screen w-full top-0 left-0 max-sm:p-6 flex-col justify-center items-center `}>
         <div
-          className="overlay bg-black opacity-85 absolute left-0 top-0 w-full h-full"
+          className="overlay bg-black opacity-95 absolute left-0 top-0 w-full h-full"
           onClick={() => {
             setModalActive(false);
           }}
         ></div>
 
-        <div className="user-options bg-black relative flex flex-col gap-3 items-center justify-center text-white z-30 border border-white/20 px-6 py-8 w-full  sm:w-3/4 lg:w-3/6 xl:w-1/4">
+        <div className="user-options bg-black relative flex flex-col gap-3 items-center justify-center text-white z-30 border border-white/30 px-6 py-8 w-full  sm:w-3/4 lg:w-3/6 xl:w-1/4">
           <button
             onClick={() => {
               setModalActive(false);
