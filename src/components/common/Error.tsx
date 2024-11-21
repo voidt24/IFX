471fbcabc00fe178fa9f3ebbc84a897003f99d2d
Andrey <devandrey98@gmail.com>
2024-11-20 21:35:39 -0600

    fixed errorMessage type

--- src/components/common/Error.tsx
+++ src/components/common/Error.tsx
@@ -1,7 +1,7 @@
 export type errorMessageType = {
-  active: false;
-  text: "";
-}
+  active: boolean;
+  text: string;
+};
 
 export default function Error({ errorMessage }: { errorMessage: errorMessageType }) {
   return <span id="error">{errorMessage.text}</span>;
