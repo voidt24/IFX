c4a32f739d998ec3fb3b138a35dec3eed473f6b9
Andrey <devandrey98@gmail.com>
2024-11-22 23:56:15 -0600

    upd. skeletons

--- src/components/common/Skeletons/HeroSkeleton.tsx
+++ src/components/common/Skeletons/HeroSkeleton.tsx
@@ -4,10 +4,10 @@ const HeroSkeleton = () => {
   return (
     <div className=" max-w-full lg:max-w-[95%] 2xl:max-w-[80%] 4K:max-w-[75%] relative px-2 mx-auto w-full overflow-hidden">
       {/* Slider content skeleton */}
-      <div className="slider-skeleton flex justify-center items-center gap-4">
+      <div className="slider-skeleton flex justify-center items-center gap-4 h-[300px] md:h-[450px] xl:h-[410px]">
         {/* Slide anterior */}
-        <div className="slide-previous relative opacity-50 w-1/5">
-          <div className="rounded-lg bg-gray-700 h-48 w-full"></div>
+        <div className="slide-previous relative opacity-50 w-1/12">
+          <div className="rounded-lg bg-gray-700 h-40 sm:h-60 xl:h-80 w-full"></div>
           <div className="absolute flex flex-col gap-2 items-center justify-center top-1/2 text-center -translate-x-1/2 -translate-y-1/2 left-1/2 w-full z-20">
             <div className="h-6 bg-gray-800 rounded-md w-1/2 mb-2"></div>
             <div className="h-6 w-1/4 bg-gray-700 rounded-md"></div>
@@ -16,7 +16,7 @@ const HeroSkeleton = () => {
 
         {/* Slide central */}
         <div className="slide-current relative w-8/12">
-          <div className="rounded-lg bg-gray-700 h-52 w-full"></div>
+          <div className="rounded-lg bg-gray-700 h-48 sm:h-64 md:h-80 lg:h-[370px] w-full"></div>
           <div className="absolute flex flex-col gap-2 items-center justify-center top-1/2 text-center -translate-x-1/2 -translate-y-1/2 left-1/2 w-full z-20">
             <div className="h-6 bg-gray-800 rounded-md w-1/2 mb-2"></div>
             <div className="h-6 w-1/4 bg-gray-700 rounded-md"></div>
@@ -24,8 +24,8 @@ const HeroSkeleton = () => {
         </div>
 
         {/* Slide siguiente */}
-        <div className="slide-next relative opacity-50 w-1/5">
-          <div className="rounded-lg bg-gray-700 h-48 w-full"></div>
+        <div className="slide-next relative opacity-50 w-1/12">
+          <div className="rounded-lg bg-gray-700 h-40 sm:h-60 xl:h-80 w-full"></div>
           <div className="absolute flex flex-col gap-2 items-center justify-center top-1/2 text-center -translate-x-1/2 -translate-y-1/2 left-1/2 w-full z-20">
             <div className="h-6 bg-gray-800 rounded-md w-1/2 mb-2"></div>
             <div className="h-6 w-1/4 bg-gray-700 rounded-md"></div>
