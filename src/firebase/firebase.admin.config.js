3e78a5c3a5a37aa00fd68a6b063bbfab52f23ae9
Andrey <devandrey98@gmail.com>
2024-11-22 19:03:59 -0600

    added admin config files

---
+++ src/firebase/firebase.admin.config.js
@@ -0,0 +1,25 @@
+import admin from "firebase-admin";
+
+const firebaseConfig = {
+  credential: admin.credential.cert({
+    type: process.env.NEXT_PUBLIC_TYPE,
+    project_id: process.env.NEXT_PUBLIC_PROJECT_ID,
+    private_key_id: process.env.NEXT_PUBLIC_PRIVATE_KEY_ID,
+    private_key: process.env.NEXT_PUBLIC_PRIVATE_KEY,
+    client_email: process.env.NEXT_PUBLIC_CLIENT_EMAIL,
+    client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
+    auth_uri: process.env.NEXT_PUBLIC_AUTH_URI,
+    token_uri: process.env.NEXT_PUBLIC_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.NEXT_PUBLIC_AUTH_PROVIDER_x509_CERT_URL,
+    client_x509_cert_url: process.env.NEXT_PUBLIC_CLIENT_x509_CERT_URL,
+    universe_domain: process.env.NEXT_PUBLIC_UNIVERSE_DOMAIN,
+  }),
+};
+if (!admin.apps.length) {
+  admin.initializeApp(firebaseConfig);
+}
+
+export const verifyToken = async (token) => {
+  const validToken = await admin.auth().verifyIdToken(token.token);
+  return validToken;
+};
