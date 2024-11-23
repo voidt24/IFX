dc421b73b0dfa4bffe6a8788109cff15f198a556
Andrey <devandrey98@gmail.com>
2024-11-22 20:08:30 -0600

    set api route to verifyToken validity

---
+++ src/app/api/auth/verifyToken/route.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server";
+import { verifyToken } from "@/firebase/firebase.admin.config";
+
+export const POST = async (req: Request) => {
+  try {
+    const body = await req.json();
+    const token = await verifyToken(body);
+    return NextResponse.json({ token });
+  } catch (err) {
+    throw err
+  }
+};
