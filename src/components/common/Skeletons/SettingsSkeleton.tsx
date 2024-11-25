ce955964592303ed0cfa8b7aceec8c9bcf0806f9
Andrey <devandrey98@gmail.com>
2024-11-24 21:39:15 -0600

    added settings skeleton

---
+++ src/components/common/Skeletons/SettingsSkeleton.tsx
@@ -0,0 +1,43 @@
+export default function SettingsSkeleton() {
+  return (
+    <div className="sm:mt-20 text-white pb-10 py-10 mx-auto md:w-[90%] xl:w-[80%] 4k:w-[60%] max-w-full max-md:px-4 animate-pulse">
+      <div>
+        <div className="mb-6">
+          <div className="flex flex-col gap-4">
+            <div className="flex justify-between">
+              <div className="bg-zinc-700 h-8 w-1/2 rounded-full"></div>
+            </div>
+            <div className="flex justify-between">
+              <div className="bg-zinc-700 h-8 w-1/3 rounded-full"></div>
+            </div>
+            <div className="flex justify-between">
+              <div className="bg-zinc-700 h-8 w-1/4 rounded-full"></div>
+            </div>
+          </div>
+          <div className="flex flex-col gap-4 mt-10">
+            <div className="flex justify-between">
+              <div className="bg-zinc-700 h-8 w-1/2 rounded-full"></div>
+            </div>
+            <div className="flex justify-between">
+              <div className="bg-zinc-700 h-8 w-1/3 rounded-full"></div>
+            </div>
+            <div className="flex justify-between">
+              <div className="bg-zinc-700 h-8 w-1/4 rounded-full"></div>
+            </div>
+          </div>
+          <div className="flex flex-col gap-4 mt-10">
+            <div className="flex justify-between">
+              <div className="bg-zinc-700 h-8 w-1/2 rounded-full"></div>
+            </div>
+            <div className="flex justify-between">
+              <div className="bg-zinc-700 h-8 w-1/3 rounded-full"></div>
+            </div>
+            <div className="flex justify-between">
+              <div className="bg-zinc-700 h-8 w-1/4 rounded-full"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
