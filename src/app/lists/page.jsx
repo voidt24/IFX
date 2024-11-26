2ea7d429e4776b290273bd93cd2b5abad1d65419
Andrey <devandrey98@gmail.com>
2024-11-25 23:30:19 -0600

    added verify token api route on first render
    useful when navigating via browser history since routes use cache on history but token should be verify all the time

--- src/app/lists/page.jsx
+++ src/app/lists/page.jsx
@@ -2,14 +2,15 @@
 import { Context } from "@/context/Context";
 import Slider from "@/components/Slider";
 import { ListsResults } from "@/components/ListsResults";
-import { database, usersCollectionName } from "@/firebase/firebase.config";
+import { database, ID_TOKEN_COOKIE_NAME, usersCollectionName, VERIFY_TOKEN_ROUTE } from "@/firebase/firebase.config";
 import { useContext, useEffect, useRef, useState } from "react";
-import {  getFieldsFromCollection } from "@/firebase/fetchMyData";
+import { getFieldsFromCollection } from "@/firebase/fetchMyData";
 import { doc, onSnapshot } from "firebase/firestore";
 import CircularProgress from "@mui/material/CircularProgress";
 import { Snackbar, Alert } from "@mui/material";
+import getCookie from "@/helpers/getCookie";
 export default function Lists() {
-  const { listActive, setListActive,firebaseActiveUser, setCheckedMedia, edit, setEdit } = useContext(Context);
+  const { listActive, setListActive, firebaseActiveUser, setCheckedMedia, edit, setEdit } = useContext(Context);
 
   const [listsNames, setListsNames] = useState();
   const [currentListData, setCurrentListData] = useState([]);
@@ -17,6 +18,7 @@ export default function Lists() {
   const [message, setMessage] = useState({ active: false, text: "" });
   const buttonRef = useRef();
   const buttonRef2 = useRef();
+  const [loader, setLoader] = useState(true);
 
   const truncatedTextStyle = {
     WebkitLineClamp: "1",
@@ -24,6 +26,33 @@ export default function Lists() {
     overflow: "hidden",
     display: "-webkit-box",
   };
+
+  useEffect(() => {
+    async function verifyToken() {
+      const authCookie = getCookie(ID_TOKEN_COOKIE_NAME);
+
+      if (!authCookie) {
+        router.push("/");
+        return;
+      }
+
+      const verify = await fetch(VERIFY_TOKEN_ROUTE, {
+        method: "POST",
+        body: JSON.stringify({ token: authCookie }),
+      });
+
+      if (!verify.ok) {
+        router.push("/");
+      }
+    }
+    try {
+      verifyToken();
+    } catch (e) {
+      router.push("/");
+    } finally {
+      setLoader(false);
+    }
+  }, []);
 
   useEffect(() => {
     if (buttonRef.current && buttonRef.current.classList.contains("active")) {
@@ -77,7 +106,8 @@ export default function Lists() {
   return (
     <div className="lists py-10 sm:py-20">
       <Slider sideControls>
-        {defaultListButtons && listsNames &&
+        {defaultListButtons &&
+          listsNames &&
           defaultListButtons.map((name, index) => {
             return (
               <button
@@ -100,7 +130,6 @@ export default function Lists() {
                   }
                   setListActive(name);
                   evt.target.scrollIntoView({ behavior: "smooth", block: "center" });
-                
                 }}
               >
                 {name}
