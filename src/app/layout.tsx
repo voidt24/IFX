e48a9e2ad242c3c6287c7794f86b4ba3a47165c6
Andrey <devandrey98@gmail.com>
2024-11-26 02:38:11 -0600

    added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,14 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="Prods" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body>
         <ContextWrapper>
           <Navbar />
