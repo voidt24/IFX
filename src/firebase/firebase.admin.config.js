bba324de05bd1350ee7df4da50d122b13c319637
Andrey <devandrey98@gmail.com>
2024-11-26 00:08:59 -0600

    updated env variables acces type

--- src/firebase/firebase.admin.config.js
+++ src/firebase/firebase.admin.config.js
@@ -2,17 +2,17 @@ import admin from "firebase-admin";
 
 const firebaseConfig = {
   credential: admin.credential.cert({
-    type: process.env.NEXT_PUBLIC_TYPE,
-    project_id: process.env.NEXT_PUBLIC_PROJECT_ID,
-    private_key_id: process.env.NEXT_PUBLIC_PRIVATE_KEY_ID,
-    private_key: process.env.NEXT_PUBLIC_PRIVATE_KEY,
-    client_email: process.env.NEXT_PUBLIC_CLIENT_EMAIL,
-    client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
-    auth_uri: process.env.NEXT_PUBLIC_AUTH_URI,
-    token_uri: process.env.NEXT_PUBLIC_TOKEN_URI,
-    auth_provider_x509_cert_url: process.env.NEXT_PUBLIC_AUTH_PROVIDER_x509_CERT_URL,
-    client_x509_cert_url: process.env.NEXT_PUBLIC_CLIENT_x509_CERT_URL,
-    universe_domain: process.env.NEXT_PUBLIC_UNIVERSE_DOMAIN,
+    type: process.env.NEXT_TYPE,
+    project_id: process.env.NEXT_PROJECT_ID,
+    private_key_id: process.env.NEXT_PRIVATE_KEY_ID,
+    private_key: process.env.NEXT_PRIVATE_KEY,
+    client_email: process.env.NEXT_CLIENT_EMAIL,
+    client_id: process.env.NEXT_CLIENT_ID,
+    auth_uri: process.env.NEXT_AUTH_URI,
+    token_uri: process.env.NEXT_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.NEXT_AUTH_PROVIDER_x509_CERT_URL,
+    client_x509_cert_url: process.env.NEXT_CLIENT_x509_CERT_URL,
+    universe_domain: process.env.NEXT_UNIVERSE_DOMAIN,
   }),
 };
 if (!admin.apps.length) {
