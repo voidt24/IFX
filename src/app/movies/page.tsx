ee44b7325ceb54fded581505a84531accf90ddf4
Andrey <devandrey98@gmail.com>
2025-01-14 11:25:49 -0600

    updated some metadata (titles)

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -1,6 +1,10 @@
-"use client";
 import AllMediaData from "@/components/common/AllMediaData";
 import { mediaProperties } from "@/helpers/mediaProperties.config";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Movies - Prods",
+};
 
 export default function Movies() {
   return <AllMediaData mediaTypeObj={mediaProperties.movie} searchCategory={"popular"} title="All Movies" />;
--- src/app/tvshows/page.tsx
+++ src/app/tvshows/page.tsx
@@ -1,7 +1,10 @@
-"use client";
 import AllMediaData from "@/components/common/AllMediaData";
 import { mediaProperties } from "@/helpers/mediaProperties.config";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "TV Shows - Prods",
+};
 export default function Tvshows() {
   return <AllMediaData mediaTypeObj={mediaProperties.tv} searchCategory={"trending"} title="All TV Shows" />;
 }
