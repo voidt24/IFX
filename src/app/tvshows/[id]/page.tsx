d95530be4ac238c2bdd2d7668d27cc487fdfc7ee
Andrey <devandrey98@gmail.com>
2025-01-17 04:44:53 -0600

    feat: generate metadata for page title

--- src/app/tvshows/[id]/page.tsx
+++ src/app/tvshows/[id]/page.tsx
@@ -1,4 +1,25 @@
 import MediaDetails from "@/components/MediaDetails";
+import { fetchDetailsData } from "@/helpers/fetchDetailsData";
+type Params = {
+  id: string;
+};
+export async function generateMetadata({ params }: { params: Params }) {
+  const { id } = params;
+  let tvTitle = "TV Details";
+
+  try {
+    const tvDetails = await fetchDetailsData("byId", "tv", id);
+    if ((tvDetails && tvDetails.name) || tvDetails.original_name) {
+      tvTitle = tvDetails.name || tvDetails.original_name;
+    }
+  } catch (error) {
+    console.error("Error fetching title:", error);
+  }
+
+  return {
+    title: tvTitle + " - Prods",
+  };
+}
 const Media = () => {
   return <MediaDetails mediaType={"tv"} />;
 };
