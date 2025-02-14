b75d732cf0250e151bbc8766018af1bfa9423c9e
Andrey <devandrey98@gmail.com>
2025-02-13 22:37:57 -0600

    fix: signature issues]

--- src/app/[mediaType]/[id]/page.tsx
+++ src/app/[mediaType]/[id]/page.tsx
@@ -5,29 +5,29 @@ import { Metadata } from "next";
 interface Params {
   params: { mediaType: string; id: string };
 }
-export async function getData(mediaType: string, id: string) {
-  let mediaTitle = "Movie Details";
-
-  try {
-    const mediaDetails = await fetchDetailsData("byId", mediaType == "movies" ? "movie" : "tv", id);
-
-    if (mediaType == "movies") {
-      if ((mediaDetails && mediaDetails.title) || mediaDetails.original_title) {
-        mediaTitle = mediaDetails.title || mediaDetails.original_title;
-      }
-    } else {
-      if ((mediaDetails && mediaDetails.name) || mediaDetails.original_name) {
-        mediaTitle = mediaDetails.name || mediaDetails.original_name;
-      }
-    }
-
-    return mediaTitle;
-  } catch (error) {
-    console.error("Error fetching title:", error);
-  }
-}
-export async function generateMetadata({ params }: Params) {
+export async function generateMetadata({ params }: Params): Promise<Metadata> {
   const { mediaType, id } = params;
+  async function getData(mediaType: string, id: string) {
+    let mediaTitle = "Movie Details";
+
+    try {
+      const mediaDetails = await fetchDetailsData("byId", mediaType == "movies" ? "movie" : "tv", id);
+
+      if (mediaType == "movies") {
+        if ((mediaDetails && mediaDetails.title) || mediaDetails.original_title) {
+          mediaTitle = mediaDetails.title || mediaDetails.original_title;
+        }
+      } else {
+        if ((mediaDetails && mediaDetails.name) || mediaDetails.original_name) {
+          mediaTitle = mediaDetails.name || mediaDetails.original_name;
+        }
+      }
+
+      return mediaTitle;
+    } catch (error) {
+      console.error("Error fetching title:", error);
+    }
+  }
   const title = await getData(mediaType, id);
 
   return {
