b9e6da9530749305eb600b5a6a7c24e1545768a5
Andrey <devandrey98@gmail.com>
2025-03-13 20:15:07 -0600

    updated banner message

--- src/components/common/SignUpBanner.tsx
+++ src/components/common/SignUpBanner.tsx
@@ -2,7 +2,7 @@ import { Context } from "@/context/Context";
 import { useContext } from "react";
 
 export default function SignUpBanner() {
-  const { setAuthModalActive } = useContext(Context);
+  const { setNoAccount, setAuthModalActive } = useContext(Context);
 
   return (
     <div className="border border-zinc-800 my-6 md:my-10 w-full  md:w-[85%] lg:w-[80%] py-6  mx-auto relative isolate overflow-hidden bg-gray-950 px-6 md:px-10 shadow-2xl rounded-3xl  lg:flex  lg:px-20 2xl:px-32 4k:w-[70%]">
@@ -21,12 +21,13 @@ export default function SignUpBanner() {
       </svg>
       <div className="mx-auto lg:text-left flex items-center justify-center w-full">
         <div className=" flex flex-col items-start justify-start lg:justify-start w-[95%] sm:w-[75%]">
-          <h2 className="text-xl  xl:text-3xl font-semibold tracking-tight text-white">Save what you like.</h2>
-          <p className="mt-6 text-pretty text-[75%] md:text-sm text-zinc-300 max-w-[90%] md:max-w-[70%]">Sign up to save movies and tv shows to favorites, watchlists and any other custom list.</p>
+          <h2 className="text-xl  xl:text-3xl font-semibold tracking-tight text-white">Your Favorites, anytime.</h2>
+          <p className="mt-6 text-pretty text-[75%] md:text-sm text-zinc-300 max-w-[90%] md:max-w-[70%]">Sign up to save media to favorites and watchlists, and enjoy access to all your content.</p>
 
           <button
             className="mt-6 px-6 py-2.5 text-[75%] md:text-sm font-semibold btn-primary"
             onClick={() => {
+              setNoAccount(true);
               setAuthModalActive(true);
             }}
           >
