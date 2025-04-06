57438e983e3e512fc1319ea1db406719025af6ac
Andrey <devandrey98@gmail.com>
2025-04-06 11:07:02 -0600

    refact: set function to accept any collection route

--- src/firebase/deleteFromFirebase.ts
+++ src/firebase/deleteFromFirebase.ts
@@ -1,9 +1,9 @@
 import { collection, deleteDoc, getDocs } from "firebase/firestore";
 import { database, usersCollectionName } from "../firebase/firebase.config";
 
-export default async function deleteFromFireStore(firebaseActiveUser: { email: string | null; uid: string | null } | null, fieldName: string, checkedMedia: (string | number)[]) {
+export default async function deleteFromFireStore(firebaseActiveUser: { email: string | null; uid: string | null } | null, fieldName: string | string[], checkedMedia: (string | number)[]) {
   if (firebaseActiveUser && firebaseActiveUser.uid) {
-    const activelistDocuments = collection(database, usersCollectionName, firebaseActiveUser?.uid, fieldName);
+    const activelistDocuments = collection(database, usersCollectionName, firebaseActiveUser?.uid, ...(Array.isArray(fieldName) ? fieldName : [fieldName]));
     const querySnapshot = await getDocs(activelistDocuments);
     querySnapshot.forEach(async (doc) => {
       if (checkedMedia.includes(doc.id.toString())) {
