36c5653b81c9d2133032bdb7eceb3a45e0eb9487
Andrey <devandrey98@gmail.com>
2025-04-06 21:26:53 -0600

    restructure layout and data gathering

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -2,7 +2,7 @@
 import { database, usersCollectionName } from "@/firebase/firebase.config";
 import React, { useEffect, useState, useContext } from "react";
 import { Context } from "@/context/Context";
-import { collection, onSnapshot } from "firebase/firestore";
+import { collection, getDocs, onSnapshot } from "firebase/firestore";
 import ConfirmDeleteModal from "@/components/common/ConfirmDeleteModal";
 import { CircularProgress } from "@mui/material";
 import Notification from "@/components/common/Notification";
@@ -11,8 +11,8 @@ import { IhistoryMedia } from "@/Types/index";
 import useVerifyToken from "@/Hooks/useVerifyToken";
 
 function History() {
-  const { firebaseActiveUser } = useContext(Context);
-  const [currentListData, setCurrentListData] = useState<IhistoryMedia[] | [] | null>(null);
+  const { firebaseActiveUser, currentId, setCurrentId } = useContext(Context);
+  const [currentListData, setCurrentListData] = useState<[string, IhistoryMedia[]][] | null>(null);
   const [message, setMessage] = useState<{ message: string; severity: "error" | "info" | "success" | "warning"; open: boolean }>({
     message: "",
     severity: "info",
@@ -20,6 +20,9 @@ function History() {
   });
   const [confirmDialog, setConfirmDialog] = useState(false);
   const [elementsToDelete, setElementsToDelete] = useState<(number | string)[]>([]);
+  const [activeIndex, setActiveIndex] = useState<number | undefined>(undefined);
+  const [activeHistoryEntry, setActiveHistoryEntry] = useState<string | null>(null);
+  const [parentActiveIndex, setParentActiveIndex] = useState<number | undefined>(undefined);
 
   useVerifyToken();
 
@@ -30,19 +33,41 @@ function History() {
     }
     //subscription to db to get real time changes
     if (database && usersCollectionName && firebaseActiveUser?.uid) {
-      const activelistDocuments = collection(database, usersCollectionName, firebaseActiveUser.uid, "history");
+      const unsubscribers: (() => void)[] = [];
 
-      const unsub = onSnapshot(activelistDocuments, (document) => {
-        const data: IhistoryMedia[] = [];
-        document.forEach((doc) => {
-          data.push(doc.data());
-        });
-        setCurrentListData(data);
+      const activelistDocuments = collection(database, usersCollectionName, firebaseActiveUser.uid, "history");
+      const historyDocResults = await getDocs(activelistDocuments);
+
+      const content: [string, IhistoryMedia[]][] = [];
+
+      historyDocResults.docs.forEach((doc) => {
+        const dateId = doc.id;
+
+        if (database && usersCollectionName && firebaseActiveUser?.uid) {
+          const contentRef = collection(database, usersCollectionName, firebaseActiveUser && firebaseActiveUser.uid, "history", dateId, "content");
+          const contentUnsub = onSnapshot(contentRef, (contentSnapshot) => {
+            const mediaItems: IhistoryMedia[] = contentSnapshot.docs.map((contentDoc) => contentDoc.data() as IhistoryMedia);
+
+            // before pushing, we delete any prev entry with that same dateId
+            const existingIndex = content.findIndex(([existingDate]) => existingDate === dateId);
+            if (existingIndex !== -1) {
+              content.splice(existingIndex, 1); // delete prev entry
+            }
+
+            if (mediaItems.length > 0) {
+              content.push([dateId, mediaItems]);
+            }
+
+            setCurrentListData([...content]);
+          });
+
+          unsubscribers.push(contentUnsub);
+        }
       });
 
       // return 'unsub' on component unmount to avoid being "subscribe" while not on this page
       return () => {
-        unsub();
+        unsubscribers.forEach((unsub) => unsub());
       };
     }
   }
@@ -60,62 +85,102 @@ function History() {
         </div>
       ) : currentListData && currentListData.length > 0 ? (
         <>
-          <div className="results z-[9] flex flex-col gap-9  ">
-            <h1 className="text-xl xl:text-2xl text-center  sticky left-0 top-0 bg-black py-4">History</h1>
+          <div className="results z-[9] flex flex-col gap-9 xl:gap-14 ">
+            <h1 className="text-xl z-[9] xl:text-2xl text-center  sticky left-0 top-0 bg-black py-4">History</h1>
             {currentListData
-              .sort((a: IhistoryMedia, b: IhistoryMedia) => (b.watchedAt ?? 0) - (a.watchedAt ?? 0))
-              .map((result: IhistoryMedia, index) => (
-                <div className=" flex items-center justify-center gap-4 border bg-zinc-900/40  border-zinc-800/90 flex-col rounded-lg  sm:w-[85%] lg:w-[80%] m-auto" key={index}>
-                  <header className=" w-full  bg-zinc-900/40 ">
-                    <p className="date text-[95%] lg:text-lg   mt-1 text-center py-2 text-zinc-500">
-                      {new Date(result.watchedAt ?? "").toLocaleDateString("en-US", {
-                        year: "numeric",
-                        month: "long",
-                        day: "numeric",
-                        hour: "2-digit",
-                        minute: "2-digit",
-                        hour12: true,
-                      })}
+              .sort((a: [string, IhistoryMedia[]], b: [string, IhistoryMedia[]]) => new Date(b[0]).getTime() - new Date(a[0]).getTime())
+              .map((result: [string, IhistoryMedia[]], index) => (
+                <div className="flex flex-col items-center justify-center gap-4" key={`date-${result[0]}`}>
+                  <header className=" w-full bg-zinc-900/90 m-auto  ">
+                    <p className="date text-[95%] lg:text-lg  px-4 mt-1 py-2 text-zinc-200">
+                      {(() => {
+                        const dateString = result[0];
+                        const parts = dateString.split("-");
+                        const year = parseInt(parts[0], 10);
+                        const month = parseInt(parts[1], 10) - 1;
+                        const day = parseInt(parts[2], 10);
+                        return new Date(year, month, day).toLocaleDateString("en-US", {
+                          year: "numeric",
+                          month: "long",
+                          day: "numeric",
+                        });
+                      })()}
                     </p>
                   </header>
+                  <div className=" flex items-center justify-center gap-6 flex-col rounded-lg sm:w-[85%] lg:w-[80%] m-auto">
+                    {result[1].map((data, childIndex) => (
+                      <div
+                        className=" relative flex flex-col lg:flex-row w-[85%] items-center justify-center lg:w-[95%]  gap-4  rounded-lg border border-zinc-800 bg-zinc-900/30 lg:hover:bg-zinc-900/60  p-4"
+                        key={`${result[0]}-${data.id}`}
+                      >
+                        <div className="relative  rounded-md md:w-[80%] lg:w-[140%] xl:w-[55%] 2xl:w-[45%] ">
+                          <img className=" block w-full rounded-md " src={data.media_type === "movie" ? `${data.backdrop_path}` : `${data.episode_image}`} alt="" />
 
-                  <div className=" flex flex-col lg:flex-row w-[85%] items-center justify-center lg:w-[95%]  gap-4  rounded-lg lg:hover:bg-zinc-900/20  p-4 ">
-                    <img
-                      className="w-full md:w-[80%] lg:w-[65%] xl:w-[55%] 2xl:w-[45%] rounded-md"
-                      src={result.media_type === "movie" ? `${result.backdrop_path}` : `${result.episode_image}`}
-                      alt=""
-                    />
+                          <span className="absolute left-2 bottom-2 text-[65%] xl:text-[75%] rounded-full px-3 lg:py-1 border bg-black/70 border-zinc-700 text-zinc-300 ">{data.media_type}</span>
+                        </div>
 
-                    <Link
-                      className="hover:underline w-full text-center sm:w-[75%]  lg:w-[95%] xl:w-[80%] 2xl:w-[50%] 4k:w-[40%] m-auto "
-                      href={`https://prods.vercel.app/${result.media_type == "tv" ? "tvshows" : "movies"}/${result.id}/watch?name=${result.title}&season=${result.season}&episode=${
-                        result.episode_number
-                      }`}
-                    >
-                      <h2 className="text-[105%] lg:text-xl text-white  ">{result.title}</h2>
-                      {result.media_type === "tv" && (
-                        <span className="text-[85%] font-normal text-zinc-300 ">
-                          <span>Season: {result.season}</span>
-                          <span> - </span>
-                          <span>Episode: {result.episode}</span>
-                        </span>
-                      )}
-                    </Link>
+                        <div className="w-full p-4 text-center sm:w-[75%]  lg:w-[95%] xl:w-[80%] 2xl:w-[50%] 4k:w-[40%] m-auto ">
+                          <h2 className="text-[105%] lg:text-xl text-white  ">{data.title}</h2>
+                          {data.media_type === "tv" && (
+                            <span className="text-[85%] font-normal text-zinc-300 ">
+                              <span>Season: {data.season}</span>
+                              <span> - </span>
+                              <span>Episode: {data.episode}</span>
+                            </span>
+                          )}
+                        </div>
+
+                        <footer className="absolute right-2 bottom-2 flex items-center justify-between text-center self-end">
+                          <button
+                            className=" rounded-full text-zinc-400 hover:text-zinc-100 hover:bg-zinc-800 px-4"
+                            onClick={() => {
+                              if (parentActiveIndex === index && activeIndex === childIndex) {
+                                setParentActiveIndex(undefined);
+                                setActiveIndex(undefined);
+                                return;
+                              }
+                              setParentActiveIndex(index);
+                              setActiveIndex(childIndex);
+                              setActiveHistoryEntry(result[0]);
+                            }}
+                            title="options"
+                          >
+                            <i className="bi bi-three-dots lg:text-[120%]"></i>
+                          </button>
+
+                          <div
+                            className={`${
+                              parentActiveIndex === index && activeIndex === childIndex ? `flex` : "hidden"
+                            } absolute right-6 text-[95%] rounded-lg bottom-7 flex-col items-center justify-between gap-2 w-32 bg-zinc-900 border border-zinc-700`}
+                          >
+                            <Link
+                              className="w-full hover:bg-zinc-800 py-2"
+                              href={`https://prods.vercel.app/${data.media_type == "tv" ? "tvshows" : "movies"}/${data.id}/watch?name=${encodeURIComponent(data.title ?? "")}${
+                                data.media_type == "tv" ? `&season=${data.season}&episode=${data.episode_number}` : ``
+                              } `}
+                              onClick={() => {
+                                setCurrentId(data.id ?? 0);
+                              }}
+                            >
+                              Watch again
+                            </Link>
+                            <button
+                              className="mt-2 w-full hover:bg-zinc-800 py-2 text-red-800 hover:text-red-700 "
+                              onClick={() => {
+                                if (firebaseActiveUser && firebaseActiveUser.uid) {
+                                  setElementsToDelete([data.media_type == "tv" ? data.episodeId?.toString() ?? "" : data.id?.toString() ?? ""]);
+                                  setConfirmDialog(true);
+                                  setActiveIndex(undefined);
+                                }
+                              }}
+                            >
+                              Delete
+                            </button>
+                          </div>
+                        </footer>
+                      </div>
+                    ))}
                   </div>
-
-                  <footer className="text-center pb-4 px-2 text-red-800 self-end ">
-                    <button
-                      onClick={() => {
-                        if (firebaseActiveUser && firebaseActiveUser.uid) {
-                          setElementsToDelete([result.media_type == "tv" ? result.episodeId?.toString() ?? "" : result.id?.toString() ?? ""]);
-                          setConfirmDialog(true);
-                        }
-                      }}
-                      className=" rounded-xl px-3 hover:bg-zinc-800 "
-                    >
-                      Delete
-                    </button>
-                  </footer>
                 </div>
               ))}
           </div>
@@ -123,9 +188,10 @@ function History() {
             <ConfirmDeleteModal
               confirmDialog={confirmDialog}
               setConfirmDialog={setConfirmDialog}
-              listName={"history"}
+              listName={[`history`, activeHistoryEntry ?? "", "content"]}
               elementsToDelete={elementsToDelete}
               setElementsToDelete={setElementsToDelete}
+              displayMessage={"history"}
               setMessage={setMessage}
             />
           )}
--- src/firebase/saveToHistory.ts
+++ src/firebase/saveToHistory.ts
@@ -1,14 +1,34 @@
 import { IhistoryMedia } from "@/Types/index";
 import { database, usersCollectionName } from "./firebase.config";
-import { doc, getDoc, setDoc } from "firebase/firestore";
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
 
 export async function saveToHistory(dataToSave: IhistoryMedia, id: number, userUid: string) {
   try {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const day = date.getDate();
+    const dateId = `${year}-${(month + 1).toString().padStart(2, "0")}-${day.toString().padStart(2, "0")}`;
+   
     const userDocRef = doc(database, usersCollectionName, userUid);
     const userDocResults = await getDoc(userDocRef);
 
     if (userDocResults.exists()) {
-      await setDoc(doc(database, usersCollectionName, userUid, "history", id.toString()), dataToSave, { merge: true });
+      const historyDocRef = doc(database, usersCollectionName, userUid, "history", dateId);
+      const mediaDocRef = doc(database, usersCollectionName, userUid, "history", dateId, "content", id.toString());
+
+      const historySnapshot = await getDoc(historyDocRef);
+      
+      if (!historySnapshot.exists()) {
+        await setDoc(historyDocRef, { createdAt: serverTimestamp() }, { merge: true });
+        await setDoc(mediaDocRef, dataToSave, { merge: true });
+      } else {
+        const mediaDocResults = await getDoc(mediaDocRef);
+
+        if (!mediaDocResults.exists()) {
+          await setDoc(mediaDocRef, dataToSave, { merge: true });
+        }
+      }
     }
   } catch (err) {
     throw err;
