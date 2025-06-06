47f764aecc3d6affb3d6f122c5eabc6e8493ac5b
Andrey <devandrey98@gmail.com>
2025-06-05 20:17:51 -0600

    update pagination number of pages

--- src/components/common/Pagination.tsx
+++ src/components/common/Pagination.tsx
@@ -1,6 +1,6 @@
 import { Dispatch, SetStateAction, useEffect, useState } from "react";
 
-const MIN_NUMBER_OF_PAGES = 8;
+const MIN_NUMBER_OF_PAGES = 5;
 
 function Pagination({
   pageActive,
@@ -30,10 +30,10 @@ function Pagination({
 
   return (
     <nav className={`flex items-center justify-center w-full px-4 ${customClasses || ""} `}>
-      <ul className="flex text-[67%] md:text-[70%] self-center  bg-[#0e1017ca] rounded-full">
+      <ul className="flex text-[75%] md:text-[70%] self-center  bg-[#0e1017ca] rounded-full">
         <li>
           <button
-            className={`flex items-center justify-center max-md:px-2 px-4 h-8 ms-0  leading-tight text-gray-200  border  border-zinc-500 rounded-s-full hover:bg-gray-600 hover:text-white transition-all duration-200 ${
+            className={`flex items-center justify-center max-md:px-4 px-6 h-8 ms-0  leading-tight text-gray-200  border  border-zinc-500 rounded-s-full hover:bg-gray-600 hover:text-white transition-all duration-200 ${
               pageActive == 1 ? " pointer-events-none text-zinc-600" : "pointer-events-auto text-gray-200  hover:text-white"
             }`}
             onClick={() => {
@@ -55,8 +55,8 @@ function Pagination({
           return (
             <li key={index}>
               <button
-                className={`flex items-center justify-center max-md:px-2.5 px-4 h-8 leading-tight  border border-zinc-500 transition-all duration-200 ${
-                  pageNumber === pageActive ? "bg-[goldenrod]  hover:bg-[goldenrod] text-white" : " hover:bg-gray-600 "
+                className={`flex items-center justify-center max-md:px-3.5 px-4 h-8 leading-tight border border-zinc-500 transition-all duration-200 ${
+                  pageNumber === pageActive ? "bg-brand-primary text-white" : " hover:bg-gray-600 "
                 }${pageNumber > numberOfPages ? " text-zinc-800 hidden pointer-events-none " : "pointer-events-auto text-gray-300 flex"}`}
                 onClick={() => {
                   setPageActive(pageNumber);
@@ -70,7 +70,7 @@ function Pagination({
 
         <li>
           <button
-            className={`flex items-center justify-center max-md:px-2 px-4 h-8 ms-0  leading-tight text-gray-200  border  border-zinc-500 rounded-e-full hover:bg-gray-600 hover:text-white transition-all duration-200 ${
+            className={`flex items-center justify-center max-md:px-4 px-6 h-8 ms-0 leading-tight text-gray-200 border border-zinc-500 rounded-e-full hover:bg-gray-600 hover:text-white transition-all duration-200 ${
               pageActive == numberOfPages ? " pointer-events-none text-zinc-600" : "pointer-events-auto text-gray-200  hover:text-white"
             }`}
             onClick={() => {
