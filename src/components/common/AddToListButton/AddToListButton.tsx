7f4ced9608fad44b0c481a6a07feead1fb82e80b
Andrey <devandrey98@gmail.com>
2025-07-02 01:43:05 -0600

    feat: component for 'add to list' buttons

---
+++ src/components/common/AddToListButton/AddToListButton.tsx
@@ -0,0 +1,33 @@
+import { MediaTypeApi } from "@/Types/mediaType";
+import { Tooltip } from "@mui/material";
+import React, { LegacyRef } from "react";
+
+function AddToListButton({
+  type,
+  wasAdded,
+  currentId,
+  mediaType,
+  mediaTypeRef,
+  onClick,
+}: {
+  type: string;
+  wasAdded: boolean;
+  currentId: number | undefined;
+  mediaType: MediaTypeApi;
+  mediaTypeRef: LegacyRef<HTMLDivElement> | null;
+  onClick: () => void;
+}) {
+  const iconClass =
+    type === "Watchlist" ? (wasAdded ? "bi bi-eye-slash text-brand-primary" : "bi bi-eye") : wasAdded && type === "Favorites" ? "bi bi-check2-all text-brand-primary" : "bi bi-check-lg";
+
+  return (
+    <Tooltip title={wasAdded ? `Delete from ${type}` : `Add to ${type}`} placement="bottom">
+      <span className="watchlist flex-col-center cursor-pointer" onClick={onClick}>
+        <i className={`text-[145%] transition-all duration-200 ${iconClass}`} data-id={currentId} ref={mediaTypeRef} data-mediatype={mediaType} id={`${type}-icon`}></i>
+        <p className="text-[75%]">{type}</p>
+      </span>
+    </Tooltip>
+  );
+}
+
+export default AddToListButton;
