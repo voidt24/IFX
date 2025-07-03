ef103635827d352f5e6a4ca9166645e00235e866
Andrey <devandrey98@gmail.com>
2025-07-02 21:51:59 -0600

    added season type

---
+++ src/Types/season.ts
@@ -0,0 +1,10 @@
+export type Season = {
+  air_date: string | null;
+  episode_count: number;
+  id: number;
+  name: string;
+  overview: string;
+  poster_path: string | null;
+  season_number: number;
+  vote_average: number;
+};
