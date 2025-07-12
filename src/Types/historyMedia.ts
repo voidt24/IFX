6df05f285739ca76bac01f4cae3d774cef5e116f
Andrey <devandrey98@gmail.com>
2025-07-11 23:15:46 -0600

    added api type to media_type

--- src/Types/historyMedia.ts
+++ src/Types/historyMedia.ts
@@ -1,7 +1,9 @@
+import { MediaTypeApi } from "./mediaType";
+
 export interface IhistoryMedia {
   id: number ;
   title?: string | null;
-  media_type?: "tv" | "movie";
+  media_type: MediaTypeApi;
   poster_path?: string | null;
   backdrop_path?: string | null;
   release_date?: string | null;
