dba7157ca84681abf053b774c7273d060ff4225b
Andrey <devandrey98@gmail.com>
2025-07-12 18:02:08 -0600

    feat: added sheet reference to removeScroll shard to prevent blocking its scroll

--- src/components/common/SlideOver.tsx
+++ src/components/common/SlideOver.tsx
@@ -2,17 +2,10 @@ import { Context } from "@/context/Context";
 import { Dispatch, ReactNode, SetStateAction, useContext } from "react";
 import { RemoveScroll } from "react-remove-scroll";
 
-type NavbarActions = {
-  name: string;
-  href: string;
-  icon: React.JSX.Element;
-  actionFunction: () => void | null;
-};
-
 export default function SlideOver({ activeState, setActiveState, children }: { activeState: boolean; setActiveState: Dispatch<SetStateAction<boolean>>; children: ReactNode }) {
-  const { containerMargin } = useContext(Context);
+  const { containerMargin, sheetsRef } = useContext(Context);
   return (
-    <RemoveScroll>
+    <RemoveScroll shards={[sheetsRef]}>
       <div
         className={`overlay !fixed right-0 transition-all duration-400 z-[9999] overflow-hidden ${activeState ? "w-[100vw] opacity-100 pointer-events-auto" : "w-0 opacity-0 pointer-events-none"}`}
         style={{ marginTop: containerMargin ? `${containerMargin}px` : undefined }}
