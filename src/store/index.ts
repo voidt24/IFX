676474acddb1562d9513bb423953455f91de345d
Andrey <devandrey98@gmail.com>
2025-07-30 18:38:27 -0600

    feat(lists): create lists slice and integrated it into store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,10 @@
 import {configureStore} from "@reduxjs/toolkit"
 import authReducer from "./slices/authSlice"
+import listsManagementReducer from "./slices/listsManagementSlice"
 export const store = configureStore({
     reducer: {
-        auth: authReducer
+        auth: authReducer,
+        listsManagement: listsManagementReducer
     }
 })
 
