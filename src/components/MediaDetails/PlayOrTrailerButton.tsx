4ecd4a3a81edceb10ab6488f2c3534261145a696
Andrey <devandrey98@gmail.com>
2025-08-13 15:57:41 -0600

    feat: create component to conditionally render play or trailer button

---
+++ src/components/MediaDetails/PlayOrTrailerButton.tsx
@@ -0,0 +1,11 @@
+import { isReleased } from "@/helpers/isReleased";
+import PlayButton from "./Buttons/PlayButton";
+import TrailerButton from "./Buttons/TrailerButton";
+import { MediaTypeApi } from "@/Types";
+import { ImediaDetailsData } from "@/Types/mediaDetails";
+
+function PlayOrTrailerButton({ mediaId, mediaType, mediaData }: { mediaId: number; mediaType: MediaTypeApi; mediaData: ImediaDetailsData }) {
+  return isReleased(mediaData.releaseDate) ? <PlayButton mediaId={mediaId} mediaType={mediaType} data={mediaData} /> : <TrailerButton id={mediaId} mediaType={mediaType} />;
+}
+
+export default PlayOrTrailerButton;
