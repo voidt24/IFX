882f99ab73bfb77b56f3d7561bcac4a37be10668
Andrey <devandrey98@gmail.com>
2025-08-19 15:27:46 -0600

    feat(redux): create pagination slice

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,13 +3,15 @@ import authReducer from "./slices/authSlice"
 import listsManagementReducer from "./slices/listsManagementSlice"
 import searchReducer from "./slices/searchSlice"
 import mediaDetailsReducer from "./slices/mediaDetailsSlice"
+import paginationReducer from "./slices/paginationSlice"
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
         listsManagement: listsManagementReducer,
         search: searchReducer,
-        mediaDetails: mediaDetailsReducer
+        mediaDetails: mediaDetailsReducer,
+        pagination: paginationReducer
     }
 })
 
---
+++ src/store/slices/paginationSlice.ts
@@ -0,0 +1,27 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface paginationState{
+    numberOfPages: number;
+    pageActive: number;
+}
+
+const initialState: paginationState = {
+    numberOfPages: 0,
+    pageActive: 0
+}
+
+export const paginationSlice = createSlice({
+    name: "pagination",
+    initialState,
+    reducers:{
+        setNumberOfPages: (state, action: PayloadAction<number>) => {
+            state.numberOfPages = action.payload
+        },
+        setPageActive: (state, action: PayloadAction<number>) => {
+            state.pageActive = action.payload
+        }
+    }
+})
+
+export const {setNumberOfPages, setPageActive} = paginationSlice.actions
+export default paginationSlice.reducer
