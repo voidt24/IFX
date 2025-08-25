c68ebccc187effbbfb4d912951078c94a2363b96
Andrey <devandrey98@gmail.com>
2025-08-25 13:23:17 -0600

    feat: create details button component

---
+++ src/components/Hero/DetailsButton.tsx
@@ -0,0 +1,27 @@
+"use client";
+import Link from "next/link";
+import { IMediaData } from "@/Types";
+import { setMediaIdPWA } from "@/store/slices/mediaDetailsSlice";
+import { useDispatch } from "react-redux";
+
+function DetailsButton({ variant, sliderData, type }: { variant: "desktop" | "mobile"; sliderData: IMediaData; type: string }) {
+  const dispatch = useDispatch();
+
+  const mobileStyles = "bg-surface-modal hover:bg-white/20 rounded-full  border border-[#ffffff4b] py-[0.5px] px-6";
+  const desktopStyles = "btn-primary text-[40%] !py-0 !px-6";
+
+  return (
+    <Link
+      className={variant === "desktop" ? desktopStyles : mobileStyles}
+      href={`${type.toLowerCase().split(" ").join("")}/${sliderData.id}`}
+      onClick={() => {
+        dispatch(setMediaIdPWA(sliderData.id));
+        sessionStorage.setItem("navigatingFromApp", "1");
+      }}
+    >
+      {variant === "desktop" ? "Details" : <i className="bi bi-caret-right-fill leading-none text-[90%]"></i>}
+    </Link>
+  );
+}
+
+export default DetailsButton;
