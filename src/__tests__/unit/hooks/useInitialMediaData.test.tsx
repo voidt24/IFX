ae81944e45e8c83f506e04d751a7fe0f1cbea88d
Andrey <devandrey98@gmail.com>
2025-08-25 00:01:15 -0600

    feat(test): implement test for home page data management hook

---
+++ src/__tests__/unit/hooks/useInitialMediaData.test.tsx
@@ -0,0 +1,65 @@
+import useInitialMediaData from "@/Hooks/useInitialMediaData";
+import { renderHook, waitFor } from "@testing-library/react";
+import { fetchInitialData } from "@/helpers/fetchInitialData";
+
+jest.mock("@/helpers/fetchInitialData");
+
+describe("data fetching custom hook test", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should resolve with empty data", async () => {
+    (fetchInitialData as jest.Mock).mockResolvedValueOnce([[]]).mockResolvedValueOnce([[]]).mockResolvedValueOnce([[]]); // tv
+
+    const { result } = renderHook(() => useInitialMediaData());
+
+    expect(result.current.isLoading).toBe(true);
+    expect(result.current.data).toStrictEqual({ movies: [], moviesHero: [], tv: [] });
+    expect(result.current.error).toBe(false);
+
+    await waitFor(() => {
+      expect(result.current.isLoading).toBe(false);
+      expect(result.current.data).toStrictEqual({ movies: [], moviesHero: [], tv: [] });
+      expect(fetchInitialData).toHaveBeenCalledTimes(3);
+    });
+  });
+
+  it("should resolve with data", async () => {
+    (fetchInitialData as jest.Mock)
+      .mockResolvedValueOnce([[{ id: 1, title: "Inception" }], 42]) // moviesHero
+      .mockResolvedValueOnce([[{ id: 2, title: "Matrix" }], 12]) // movies
+      .mockResolvedValueOnce([[{ id: 3, title: "Breaking Bad" }], 5]); // tv
+
+    const { result } = renderHook(() => useInitialMediaData());
+
+    expect(result.current.isLoading).toBe(true);
+    expect(result.current.data).toStrictEqual({ movies: [], moviesHero: [], tv: [] });
+    expect(result.current.error).toBe(false);
+
+    await waitFor(() => {
+      expect(result.current.isLoading).toBe(false);
+      expect(result.current.data).toStrictEqual({
+        moviesHero: [{ id: 1, title: "Inception" }],
+        movies: [{ id: 2, title: "Matrix" }],
+        tv: [{ id: 3, title: "Breaking Bad" }],
+      });
+      expect(fetchInitialData).toHaveBeenCalledTimes(3);
+    });
+  });
+
+  it("should handle fetch error", async () => {
+    (fetchInitialData as jest.Mock).mockRejectedValueOnce(new Error("api error"));
+
+    const { result } = renderHook(() => useInitialMediaData());
+
+    expect(result.current.isLoading).toBe(true);
+    expect(result.current.data).toStrictEqual({ movies: [], moviesHero: [], tv: [] });
+    expect(result.current.error).toBe(false);
+
+    await waitFor(() => {
+      expect(result.current.isLoading).toBe(false);
+      expect(result.current.error).toBe(true);
+    });
+  });
+});
