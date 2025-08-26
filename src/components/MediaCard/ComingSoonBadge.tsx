b98f0299589c949cc77241888e028f71b3edad1d
Andrey <devandrey98@gmail.com>
2025-08-25 18:15:37 -0600

    refact: moved cooming soon badge render decision to its parent component

--- src/components/MediaCard/ComingSoonBadge.tsx
+++ src/components/MediaCard/ComingSoonBadge.tsx
@@ -1,14 +1,12 @@
 import formatReleaseDate from "@/helpers/formatReleaseDate";
 
 function ComingSoonBadge({ release_date, first_air_date }: { release_date: string | undefined; first_air_date: string | undefined }) {
-  const notReleasedYet = (release_date && new Date(release_date).getTime() > Date.now()) || (first_air_date && new Date(first_air_date).getTime() > Date.now());
-
-  return notReleasedYet ? (
+  return (
     <div className="cooming-soon block uppercase absolute bottom-10 text-center w-full left-0 right-0 z-[2] backdrop-blur-xl bg-brand-primary/20 [text-shadow:1px_1px_3px_black] py-1 text-content-primary font-bold text-[70%] lg:text-[80%]">
       coming soon
       <div className="block text-[85%]">{formatReleaseDate(release_date || first_air_date || "")}</div>
     </div>
-  ) : null;
+  );
 }
 
 export default ComingSoonBadge;
--- src/components/MediaCard/MediaCard.tsx
+++ src/components/MediaCard/MediaCard.tsx
@@ -7,6 +7,7 @@ import MediaTypeBadge from "./MediaTypeBadge";
 
 function MediaCard({ result, poster, vote, canBeEdited }: { result: IMediaData; poster: string; vote: string | undefined; canBeEdited: boolean }) {
   const { release_date, first_air_date } = result;
+  const notReleasedYet = (release_date && new Date(release_date).getTime() > Date.now()) || (first_air_date && new Date(first_air_date).getTime() > Date.now());
 
   return (
     <div className={`card rounded-md border-[3px] border-transparent z-[1] lg:hover:border lg:hover:border-brand-primary transition-all duration-200 `}>
@@ -17,7 +18,7 @@ function MediaCard({ result, poster, vote, canBeEdited }: { result: IMediaData; 
 
         <img src={poster} alt="" className={`rounded-md`} width={780} height={1170} />
 
-        <ComingSoonBadge release_date={release_date} first_air_date={first_air_date} />
+        {notReleasedYet ? <ComingSoonBadge release_date={release_date} first_air_date={first_air_date} /> : null}
 
         {canBeEdited ? <MediaTypeBadge mediaType={result.media_type} /> : null}
       </div>
