72022dc1747c406ef30b1c117fc23ace7749f35f
Andrey <devandrey98@gmail.com>
2025-10-13 22:29:23 -0600

    refact: use helper to resolve url

--- src/helpers/fetchDetailsData.js
+++ src/helpers/fetchDetailsData.js
@@ -1,31 +1,13 @@
-import { API_KEY, apiUrl } from "./api.config";
 import { getFromCache, saveToCache } from "./cache/cache";
 import { ONE_MONTH, THREE_DAYS } from "./constants";
+import { resolveFetchURL } from "./resolveFetchURL";
 
 export const fetchDetailsData = async (typeOfSearch, mediaType, id) => {
   if (!id) throw new Error("id undefined");
 
   const CACHEURL = `${mediaType}-${typeOfSearch}-${id}`;
-  let url;
-  let validTime = ONE_MONTH;
-
-  switch (typeOfSearch) {
-    case "byId":
-      url = `${apiUrl}${mediaType}/${id}?api_key=${API_KEY}`;
-      break;
-    case "similar":
-      url = `${apiUrl}${mediaType}/${id}/similar?api_key=${API_KEY}`;
-      break;
-    case "cast":
-      url = `${apiUrl}${mediaType}/${id}/credits?api_key=${API_KEY}&language=en-US&page=1`;
-      break;
-    case "reviews":
-      url = `${apiUrl}${mediaType}/${id}/reviews?api_key=${API_KEY}`;
-      validTime = THREE_DAYS;
-      break;
-    default:
-      break;
-  }
+  let url = resolveFetchURL(typeOfSearch, mediaType, id);
+  let validTime = typeOfSearch === "reviews" ? THREE_DAYS : ONE_MONTH;
 
   const getFromApi = async () => {
     try {
