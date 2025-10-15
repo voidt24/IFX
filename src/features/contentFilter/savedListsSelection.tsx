3efccd14ab6473738ca284aebcb8cf7d7359d0e4
Andrey <devandrey98@gmail.com>
2025-10-15 15:09:40 -0600

    refact: delete slider usage

--- src/features/contentFilter/savedListsSelection.tsx
+++ src/features/contentFilter/savedListsSelection.tsx
@@ -1,6 +1,4 @@
 "use client";
-import Slider from "@/components/Slider/Slider";
-import { useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "@/store";
 import { setCheckedMedia, setEdit } from "@/store/slices/listsManagementSlice";
@@ -9,8 +7,6 @@ export const defaultListButtons = ["favorites", "watchlist", "watched"];
 
 function SavedListOptions() {
   const { edit } = useSelector((state: RootState) => state.listsManagement);
-  const buttonRef = useRef<HTMLButtonElement>(null);
-  const buttonRef2 = useRef<HTMLButtonElement>(null);
   const router = useRouter();
   const searchParams = useSearchParams();
   const params = new URLSearchParams(searchParams);
@@ -18,40 +14,39 @@ function SavedListOptions() {
   const dispatch = useDispatch();
 
   return (
-    <Slider>
-      {defaultListButtons &&
-        defaultListButtons.map((name, index) => {
-          return (
-            <button
-              key={index}
-              type="button"
-              className={`rounded-full ${list == name ? "bg-brand-primary py-1 px-4" : ""}`}
-              onClick={(evt) => {
-                params.set("selected", name);
-                router.replace(`?${params.toString()}`);
+    <div className="w-full flex items-center justify-center gap-4 sm:gap-7 xl:gap-10 ">
+      {defaultListButtons.map((name, index) => {
+        return (
+          <button
+            key={index}
+            type="button"
+            className={`transition-all rounded-full py-1 px-4 ${list == name ? "bg-brand-primary " : ""}`}
+            onClick={(evt) => {
+              params.set("selected", name);
+              router.replace(`?${params.toString()}`);
 
-                if (edit && list != name) {
-                  dispatch(setEdit(false));
-                  const card = document.querySelectorAll(".card");
-                  card.forEach((card) => {
-                    if (card instanceof HTMLElement) {
-                      card.style.border = "3px solid transparent";
-                      const cardImg = card.querySelector("img");
-                      if (cardImg instanceof HTMLImageElement) {
-                        cardImg.style.filter = "none";
-                        cardImg.style.transform = "scale(1)";
-                      }
+              if (edit && list != name) {
+                dispatch(setEdit(false));
+                const card = document.querySelectorAll(".card");
+                card.forEach((card) => {
+                  if (card instanceof HTMLElement) {
+                    card.style.border = "3px solid transparent";
+                    const cardImg = card.querySelector("img");
+                    if (cardImg instanceof HTMLImageElement) {
+                      cardImg.style.filter = "none";
+                      cardImg.style.transform = "scale(1)";
                     }
-                  });
-                  dispatch(setCheckedMedia([]));
-                }
-              }}
-            >
-              {name}
-            </button>
-          );
-        })}
-    </Slider>
+                  }
+                });
+                dispatch(setCheckedMedia([]));
+              }
+            }}
+          >
+            {name}
+          </button>
+        );
+      })}
+    </div>
   );
 }
 
