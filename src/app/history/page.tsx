b728e1cb34be1b2fbc1489d88751d84e3a069da3
Andrey <devandrey98@gmail.com>
2025-10-15 17:00:37 -0600

    refact: add page title

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -1,8 +1,9 @@
+import { APP_NAME } from "@/helpers/api.config";
 import History from "@/views/History";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "History - IFX",
+  title: `History - ${APP_NAME}`,
 };
 function HistoryPage() {
   return <History />;
--- src/app/lists/page.tsx
+++ src/app/lists/page.tsx
@@ -1,6 +1,11 @@
 import { Suspense } from "react";
 import Lists from "@/views/Lists";
+import { Metadata } from "next";
+import { APP_NAME } from "@/helpers/api.config";
 
+export const metadata: Metadata = {
+  title: `My lists - ${APP_NAME}`,
+};
 export default function ListsPage() {
   return (
     <Suspense fallback={<div>Loading list options...</div>}>
