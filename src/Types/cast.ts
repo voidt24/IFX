397ea6a68a2f2c5091fbfef4bb19f5a7a5b0fa49
Andrey <devandrey98@gmail.com>
2025-10-15 13:26:40 -0600

    feat: add cast interface

---
+++ src/Types/cast.ts
@@ -0,0 +1,13 @@
+export interface ICast {
+  adult?: boolean;
+  character?: string;
+  credit_id?: string;
+  gender?: number | null;
+  id: number;
+  known_for_department?: string;
+  name?: string;
+  order?: number;
+  original_name?: string;
+  popularity?: number;
+  profile_path?: string | null;
+}
