a01ce15e38b8f746a61c307b4a1d3a05a0638c0b
Andrey <devandrey98@gmail.com>
2025-11-01 02:41:32 -0600

    update footer

--- src/components/common/Footer/Footer.tsx
+++ src/components/common/Footer/Footer.tsx
@@ -1,12 +1,16 @@
 function Footer() {
   return (
-    <footer className="max-sm:hidden bg-black/50 shadow-sm text-content-muted py-4 text-center mt-10 relative">
-      <span className="text-[70%] ">
-        © {new Date().getFullYear().toString()}{" "}
+    <footer className=" bg-black/50 shadow-sm text-content-muted py-4 text-center mt-10 relative text-[85%] ">
+      <span className="">
+        ©{" "}
         <a href="/" className="hover:underline">
           IFX
         </a>
       </span>
+      <p>
+        &lt;&gt; by
+        <span className="text-brand-primary"> Andrey T</span> {new Date().getFullYear().toString()}{" "}
+      </p>
     </footer>
   );
 }
