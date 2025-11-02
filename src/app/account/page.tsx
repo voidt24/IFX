853e72272d719d10aed94cb39bc98104ecfd0390
Andrey <devandrey98@gmail.com>
2025-11-01 23:48:45 -0600

    refact/style: set page to use full height in mobile and add close button

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,21 +1,26 @@
 "use client";
 import Banner from "@/components/Banner/Banner";
-import Wrapper from "@/components/common/Wrapper/Wrapper";
 import useVerifyToken from "@/Hooks/useVerifyToken";
 import Settings from "@/components/settings/Settings";
 import { useSelector } from "react-redux";
 import { RootState } from "@/store";
 import useHideDrawers from "@/Hooks/useHideDrawers";
+import useIsMobile from "@/Hooks/useIsMobile";
+import MobileCloseButton from "@/components/MediaDetails/Buttons/MobileCloseButton";
 
 export default function Profile() {
   const auth = useSelector((state: RootState) => state.auth);
   const { profileData } = auth;
+  const isMobile = useIsMobile(768);
+  const { containerMargin } = useSelector((state: RootState) => state.ui);
 
   useVerifyToken();
   useHideDrawers();
 
   return (
-    <Wrapper>
+    <div className="wrapper relative max-lg:z-[999] z-[99] bg-black max-md:!pt-16 min-h-screen" style={{ ...(!isMobile ? { marginTop: containerMargin ? `${containerMargin}px` : undefined } : {}) }}>
+      {isMobile && <MobileCloseButton variant="profile" />}
+
       <div className="flex-col-center gap-6 ">
         <span className="w-full">
           <Banner />
@@ -26,6 +31,6 @@ export default function Profile() {
           <Settings />
         </div>
       </div>
-    </Wrapper>
+    </div>
   );
 }
