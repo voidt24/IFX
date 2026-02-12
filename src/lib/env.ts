8d3c279384650d831191987baf63b40e933d9acd
Andrey <devandrey98@gmail.com>
2026-02-11 20:00:26 -0600

    feat: code to get base url

---
+++ src/lib/env.ts
@@ -0,0 +1,11 @@
+export const getBaseUrl = () => {
+  if (typeof window !== "undefined") {
+    return window.location.origin;
+  }
+
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}`;
+  }
+
+  return "http://localhost:3000";
+};
