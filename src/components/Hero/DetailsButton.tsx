80bbabfaf728d9b4b90e190e710a88b4e6db8786
Andrey <devandrey98@gmail.com>
2026-05-21 00:06:22 -0600

    style: change detail button style in hero

--- src/components/Hero/DetailsButton.tsx
+++ src/components/Hero/DetailsButton.tsx
@@ -4,22 +4,21 @@ import { IMediaData } from "@/Types";
 import { setMediaIdPWA } from "@/store/slices/mediaDetailsSlice";
 import { useDispatch } from "react-redux";
 
-function DetailsButton({ variant, sliderData, type }: { variant: "desktop" | "mobile"; sliderData: IMediaData; type: string }) {
+function DetailsButton({ sliderData, type }: { sliderData: IMediaData; type: string }) {
   const dispatch = useDispatch();
-
-  const mobileStyles = "bg-surface-modal hover:bg-white/20 rounded-full  border border-[#ffffff4b] py-[0.5px] px-6";
-  const desktopStyles = "btn-primary text-[40%] !py-0 !px-8";
 
   return (
     <Link
-      className={variant === "desktop" ? desktopStyles : mobileStyles}
+      className={`btn-primary text-[40%] !py-0 !px-8 !bg-[#1d1d1d] !border-solid !border !border-white/20 !text-white`}
       href={`${type.toLowerCase().split(" ").join("")}/${sliderData.id}`}
       onClick={() => {
         dispatch(setMediaIdPWA(sliderData.id));
         sessionStorage.setItem("navigatingFromApp", "1");
       }}
     >
-      {variant === "desktop" ? "Details" : <i className="bi bi-caret-right-fill leading-none text-[90%]"></i>}
+      <span>
+        <i className="bi bi-info-circle mr-1"></i> Details
+      </span>
     </Link>
   );
 }
